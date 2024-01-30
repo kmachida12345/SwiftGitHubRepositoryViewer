354d48134a18ffd4546e4c080892f700496b9a61
kohei machida <kmachida12345@gmail.com>
2024-01-30 15:33:34 +0900

    Create project

---
+++ SwiftGitHubRepositoryViewer/SwiftGitHubRepositoryViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftGitHubRepositoryViewerApp.swift
+//  SwiftGitHubRepositoryViewer
+//
+//  Created by 町田康平 on 2024/01/30.
+//
+
+import SwiftUI
+
+@main
+struct SwiftGitHubRepositoryViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
